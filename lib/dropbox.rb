01145dd057567a4f2ebe9c44c791566d88a7d607
Dylan Waits <dylan@waits.io>
2016-06-05 17:40:00 -0700

    Add Client class and ClientError error class

--- lib/dropbox.rb
+++ lib/dropbox.rb
@@ -1,3 +1,23 @@
-class Dropbox
+module Dropbox
+  class Client
+    def initialize(access_token)
+      unless access_token =~ /^[a-z0-9_-]{64}$/i
+        raise ClientError.new('Access token is invalid.')
+      end
 
+      @access_token = access_token
+    end
+  end
+
+  class ClientError < StandardError
+    attr_reader :message
+
+    def initialize(message=nil)
+      @message = message
+    end
+
+    def to_s
+      @message.to_s
+    end
+  end
 end
--- test.rb
+++ test.rb
@@ -3,6 +3,21 @@ require File.expand_path '../lib/dropbox', __FILE__
 
 class DropboxTest < Minitest::Test
   def test_class
-    assert Dropbox.is_a?(Class)
+    assert Dropbox::Client.is_a?(Class), 'Dropbox::Client is not a Class'
+  end
+
+  def test_initialize
+    dbx = Dropbox::Client.new('1234567890abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ_-')
+    assert dbx.is_a?(Dropbox::Client)
+  end
+
+  def test_initialize_error
+    assert_raises(Dropbox::ClientError) do
+      dbx = Dropbox::Client.new('')
+    end
+
+    assert_raises(Dropbox::ClientError) do
+      dbx = Dropbox::Client.new(nil)
+    end
   end
 end
