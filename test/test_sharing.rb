e2d1e8d564f4e9ad340323f74a63064b62eab404
Scott Patten <scott@scottpatten.ca>
2016-09-13 17:03:03 -0700

    Adding tests for `share_folder` and `add_folder_member`

---
+++ test/test_sharing.rb
@@ -0,0 +1,19 @@
+require 'test_helper'
+
+class DropboxSharingTest < Minitest::Test
+  def setup
+    @client = Dropbox::Client.new('super-fake-access-token-1234567890000000000000000000000000000000')
+  end
+
+  def test_share_folder
+    stub_request(:post, url('sharing/share_folder')).to_return(stub('share_folder'))
+    share = @client.share_folder('/test/share_me')
+    assert_equal '/test/share_me', share.path_lower
+  end
+
+  def test_add_folder_member
+    stub_request(:post, url('sharing/add_folder_member')).to_return(stub('null'))
+    result = @client.add_folder_member(shared_folder_id: '123123', members: %w(one@example.com two@example.com))
+    assert_equal nil, result
+  end
+end
